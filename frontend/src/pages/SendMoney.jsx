b5726645ac61e8eda2748e1d34d917f5793bff02
Sargun Singh Sethi <100695155+SargunSinghSethi@users.noreply.github.com>
2024-04-19 21:46:56 +0530

    Updated SendMoney

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -13,7 +13,7 @@ export const SendMoney = () => {
         <div className="border h-min text-card-foreground max-w-md p-4 space-y-8 w-96 bg-white shadow-lg rounded-lg">
             <div className="flex flex-col space-y-1.5 p-2">
                 <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 cursor-pointer transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-125" onClick={()=>navigate("/dashboard")}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 cursor-pointer transition ease-in-out delay-100 hover:-translate-x-1 hover:scale-100" onClick={()=>navigate("/dashboard")}>
                         <path fillRule="evenodd" d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
                     </svg>
                 </div>
